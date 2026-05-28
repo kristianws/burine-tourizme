b57a0e0948d916bbeb8fd6838873e38e24f9745f
kristianws <kristianwjya@gmail.com>
2026-05-28 16:46:37 +0700

    code: add route for login and register, alse add comment about what it is

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,18 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+/**
+ * Routes API tanpa token
+ */
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-  return $request->user();
+/**
+ * Routes API dengan token
+ */
+Route::middleware('auth:sanctum')->group(function() {
+  Route::post('/logout', [AuthController::class, 'logout']);
+
 });
 
 
