c7f6d8cf1a2f37776311e448be25621e2fc29831
kristianws <kristianwjya@gmail.com>
2026-06-07 18:59:45 +0700

    make a controller for bisnis owner model

---
+++ app/Http/Controllers/BisnisOwnerController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BisnisOwner;
+use App\Http\Requests\RegisBisnisOwner;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+
+class BisnisOwnerController extends Controller
+{
+
+  // fungsi untuk menyetujui bisnis owner
+  public function approvedBisnisOwner(BisnisOwner $bisnisOwner) {
+    if($bisnisOwner->status !== 'pending') {
+      return $this->successResponse('Bisnis owner sudah diproses sebelumnya', 400);
+    }
+  
+    $bisnisOwner->update([
+      'status' => 'approved',
+      'verification_status' => true,
+      'verification_at' => now(),
+      'verification_notes' => null
+    ]);
+
+    return $this->successResponse(
+      $bisnisOwner,
+      'Bisnis owner berhasil disetujui',
+      200
+    );
+  }
+
+  // fungsi untuk menolak bisnis owner
+  public function rejectBisnisOwner(BisnisOwner $bisnisOwner, Request $request) {
+    if($bisnisOwner->status !== 'pending') {
+      return $this->successResponse('Bisnis owner sudah diproses sebelumnya', 400);
+    }
+
+    $request->validate([
+      'verification_notes' => 'required|string'
+    ]);
+
+    $bisnisOwner->update([
+      'status' => 'rejected',
+      'verification_status' => false,
+      'verification_at' => now(),
+      'verification_notes' => $request->verification_notes
+    ]);
+
+    return $this->successResponse(
+      $bisnisOwner,
+      'Bisnis owner ditolak',
+      200
+    );
+  }
+
+  // fungsi untuk register bisnis owner
+  public function RegisterBisnisOwner(RegisBisnisOwner $request)
+  {
+    $validated = $request->validated();
+
+    if ($request->hasFile('ktp_photo')) {
+      $ktpPhoto = $request->file('ktp_photo')->store('uploads/ktp_images', 'public');
+
+      $url = Storage::url($ktpPhoto);
+
+      $validated['ktp_photo'] = $url;
+
+      $bisnisOwner = BisnisOwner::create($validated);
+
+      return $this->successResponse(
+        data: $bisnisOwner,
+        message: 'Bisnis owner registered successfully',
+        code: 201
+      );
+    }
+
+    return $this->errorResponse(
+      message: 'Wajib Upload Foto KTP',
+      code: 400
+    );
+  }
+}
